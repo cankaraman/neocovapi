513ce4f313786271c41c3b5dee25cccd29ed4b40
can.karaman <can.karaman@droidsolutions.de>
2021-11-07 01:23:50 +0100

    add server-side filtering

--- api_resources/patients.py
+++ api_resources/patients.py
@@ -3,22 +3,53 @@ from flask_restful import Resource, Api
 
 
 from services import db_service
+from datetime import datetime
 
 
 class Patients(Resource):
     def get(self):
-        # return {'hello': 'world'}
         try:
-            # Check if ID was passed to URL query
-            # patient_id = request.args.get('id')
-            # if patient_id:
-            #     patient = patients_ref.document(patient_id).get()
-            #     return jsonify(patient.to_dict()), 200
-            # else:
-            args = request.args.get('arg1')
-            print(args)
-            all_patients = [doc.to_dict()
-                            for doc in db_service.patients_ref.stream()]
-            return make_response(jsonify(all_patients), 200)
+            status = request.args.get('status')
+            fromDate = request.args.get('fromDate')
+            toDate = request.args.get('toDate')
+            print(status)
+
+            docs_ref = db_service.patients_ref
+
+            if not (status or fromDate or toDate):
+                docs_ref = self.__get_all_patients(docs_ref)
+
+            if status:
+                docs_ref = self. __get_patients_with_status(docs_ref, status)
+
+            if toDate:
+                docs_ref = self. __get_patients_to(docs_ref, toDate)
+
+            if fromDate:
+                docs_ref = self. __get_patients_from(docs_ref, fromDate)
+
+            response_docs = [doc.to_dict() for doc in docs_ref.stream()]
+
+            return make_response(jsonify(response_docs), 200)
         except Exception as e:
-            return f"An Error Occured: {e}"
+            if type(e) is ValueError:
+                return make_response(jsonify('invalid argument'), 400)
+            else:
+                return make_response(jsonify('server error'), 500)
+
+    def __get_all_patients(self, ref):
+        return ref
+
+    def __get_patients_to(self, ref, toDate):
+        toDate = datetime.strptime(toDate, '%Y-%m-%d')
+        return ref.where(u'updated', u'<=', toDate)
+
+    def __get_patients_from(self, ref, fromDate):
+        fromDate = datetime.strptime(fromDate, '%Y-%m-%d')
+        return ref.where(u'updated', u'>=', fromDate)
+
+    def __get_patients_with_status(self, ref, status):
+        return ref.where(u'status', u'==', status)
+
+    def put(self):
+        raise NotImplementedError()
